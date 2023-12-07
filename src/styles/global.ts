b4c9ad5750369ec2fd28779295f287318eb3a5d0
Lidiane Faria <lidianerdefaria@gmail.com>
2023-12-06 21:37:33 -0300

    chore: global styles setup

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  passing: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #333;
+  color: #fff;
+}
+`;
