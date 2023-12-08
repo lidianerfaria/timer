7284fc5db0b0526de3bc42cc6321367c47ed7dc1
Lidiane Faria <lidianerdefaria@gmail.com>
2023-12-08 07:56:13 -0300

    chore: route layout setup

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import History from './pages/History'
+import DefaultLayout from './layouts/Default'
+import Home from './pages/Home'
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" />
-      <Route path="history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
