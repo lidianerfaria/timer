b7cd999400f26d184207553bf0ba4db5b0be236f
Lidiane Faria <lidianerdefaria@gmail.com>
2023-12-27 21:07:26 -0300

    refactor: segregate actions

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -1,5 +1,10 @@
 import { ReactNode, createContext, useReducer, useState } from 'react'
-import cyclesReducer, { ActionTypes, Cycle } from '../reducers/cycles'
+import cyclesReducer, { Cycle } from '../reducers/cycles/reducer'
+import {
+  addNewCycleAction,
+  interruptCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from '../reducers/cycles/actions'
 
 interface CreateCycleData {
   task: string
@@ -39,12 +44,7 @@ const CyclesContextProvider = ({ children }: CyclesContextProviderProps) => {
   }
 
   const markCurrentCycleAsFinished = () => {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId,
-      },
-    })
+    dispatch(markCurrentCycleAsFinishedAction())
   }
 
   const createNewCycle = (data: CreateCycleData) => {
@@ -55,22 +55,12 @@ const CyclesContextProvider = ({ children }: CyclesContextProviderProps) => {
       startDate: new Date(),
       task: data.task,
     }
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      },
-    })
+    dispatch(addNewCycleAction(newCycle))
     setAmountSecondsPassed(0)
   }
 
   const interruptCurrentCycle = () => {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId,
-      },
-    })
+    dispatch(interruptCurrentCycleAction())
   }
 
   return (
